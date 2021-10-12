03cb340cb80f2cb8e0411637a8f3f7be0de005ec
Guillermo Loaysa <guillermo.loaysa.extern@diconium.com>
2021-10-12 12:14:07 +0200

    feat(): add images to popup.content

--- src/app/popup.content.ts
+++ src/app/popup.content.ts
@@ -3,43 +3,43 @@ import { PopupContent } from './popup-content.interface';
 export const popupContent: PopupContent[] = [
   {
     name: 'The Daily News',
-    url: 'https://cvws.icloud-content.com/B/ASyflrD44OOywSZPg1lQ4t6LoW2BAUihJvnAHULG5wEmY6lafVdi95le/thedailyredcar.jpg?o=Agyd_At-XXW1Mrj_zjvx3sMt4hesReN1VXma7zX18OCS&v=1&x=3&a=CAoggNo2PgQ1_dCFxpaNUgXxCn2Zmmg17_vEpHo5z9PphtkSbRC0p7PXxi8Y1J7q18YvIgEAUgSLoW2BWgRi95leaiYr-mHO4C_qj6_VIIbnObj4qtwws28YnYkV5tCf_mqtbv9HtYMa7HIm7Aztx0I6ioZLT3gr_X3rfmUCisCf9A3dFR4-CfSRb45t3--JejY&e=1633882378&fl=&r=384fcb9f-1491-402b-93aa-df63ced5786b-1&k=Nd_MAkStYIxCpTl8GCFB1A&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=K67hDbEJmJKKies3N9TyrSgaZ24&cd=i',
+    url: '/assets/images/old_school/popup/thedailyredcar.jpg',
     width: 370,
     height: 574
   },
   {
     name: 'News',
-    url: 'https://cvws.icloud-content.com/B/ASm4QyB2J71_iZR7FJM3svNfsvx_AX_OXmfS7eNAvDl0qAyU0hNdHmnE/Product.jpeg?o=AmdU0NU_bpvEsHP-2JeknZ1Mr8FSdPoOfOd1_L9xbURw&v=1&x=3&a=CAogJjBJOptNHjuQUUdigQ4U1o0oZFMZvvmO5HC1FxqXIuASbRCbuYnYxi8Yu7DA2MYvIgEAUgRfsvx_WgRdHmnEaib-7bJi_a67aDjccujWwkDgtUZfAik0-E7qmuuFbFCQmX8-DDupN3ImY2EuYTOROfI-H67E3QGyEIdxRSp581FSmddhbRTX3joWk0MUbmE&e=1633883789&fl=&r=c9407f2e-b4fc-4fd2-b1ba-23ee30f7030e-1&k=svgnhJmZ-rQYY_8yT0yUEw&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=BwfbTP4qkZYT1Xh_aLLqcksnUkU&cd=i',
+    url: '/assets/images/old_school/popup/news.jpeg',
     width: 800,
     height: 500
   },
   {
     name: 'Washington Post',
-    url: 'https://cvws.icloud-content.com/B/AbllS5KIKDDI9hImCyLDzrHlRCI9AT_cIUk00uXYRwVuRaPlwSTFICl3/newspaper-generator.jpeg?o=At_G8x2dwL4h8VgRo7Y2T8oWsjSFJ8RpLv3LN3Wwh6Y2&v=1&x=3&a=CAogylhG4PdND9fpFMdpLRFUjzMDdBERC1cyrI3mggtn__4SbRDsiI_Yxi8YjIDG2MYvIgEAUgTlRCI9WgTFICl3aiY3LYg8bW1dRBJO3TBBwtIPduaqs1xHBiAY98YF8nIy28C2WYCBD3ImtzGIe3o_6QlgrYu2awAVMBRYu64a1GlO6IaEi6rIhV1Ccqcgcoo&e=1633883881&fl=&r=58b0e65a-bc4d-429e-8100-afd8f49babc4-1&k=dgsd_pFL2VJIszYqZaK-mQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=T4LhelphVqoTFSrANA0fgaI7CGk&cd=i',
+    url: '/assets/images/old_school/popup/washington-post.jpeg',
     width: 500,
     height: 328
   },
   {
     name: 'Breaking news',
-    url: 'https://cvws.icloud-content.com/B/AfRrQqAnfp4blVY0HArkiqJM77n9AbiWcsFKxdd0gjfcKXQ8z-qejQVw/breaking-news.png?o=Avbx2vUk_zTCitcqw4nEJijIJxo8xUF3WMlQvv40mbsn&v=1&x=3&a=CAogMd5ieg5cleJ7VWLiizyTIkPu7AVBbhzdkxo5IibvPukSbRDd-pLYxi8Y_fHJ2MYvIgEAUgRM77n9WgSejQVwaiaW5a5O41qUNhGR27DxdsDfjQIsftaeT9XtpdLlpqogEu1lWnAkFHImIemdMxVaDp5_XKxg07kGSKRNWbXfqG-7MCHyMkjFO54g7zDNz4k&e=1633883945&fl=&r=d136e893-9797-4018-822a-78fa9fac1c7c-1&k=SbDwLlwnvziCvWNk7QPNuQ&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=c3jx3V33382Yfonh7jG0lKtHcak&cd=i',
+    url: '/assets/images/old_school/popup/breaking-news.png',
     width: 750,
     height: 450
   },
   {
     name: 'BBC News',
-    url: 'https://cvws.icloud-content.com/B/ARHFf04Gj8xsxaL3YsrHNVMNCeo6AWrCPQACJ_ztCPG3NntC_85rOyLG/Screenshot+2021-10-10+at+17.16.51.png?o=Ah9WbgbzssROvhqte5N44K_q8a9L4nCFgIVZdQItrskK&v=1&x=3&a=CAogxax-81PE3E7sV0yMDKsHaJqNmRaV9ZGSr3U4gi-gpMASbRDrkJfYxi8Yi4jO2MYvIgEAUgQNCeo6WgRrOyLGaiaSM-3HuDVavDxRGfaFy5z8n9Pac-ed5GYCeJ15bU-AvB1ZxEdWWXImNEWzcN56dx2xBlbWHgLv-uKW9z2RTlgdIJU_304SdP7WolNmsp0&e=1633884013&fl=&r=0f45d306-c34f-4a9b-9dcd-4acaa88091b2-1&k=cC2Bxw6xiPHlyBwUXayyzw&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=uZ1njWFRZ4Is0wFltXZXHajRLNQ&cd=i',
+    url: '/assets/images/old_school/popup/bbc-news.png',
     width: 815,
     height: 670
   },
   {
     name: 'Azaña Notice',
-    url: 'https://cvws.icloud-content.com/B/AZ88-cIYZ_YekHh0FVjhoQ2Yfbz6ATvHpxVRfNZWuPDPe_6XJJHLQFxu/signs-no-trespassing-generator.jpg?o=Avans1ZfbtFGp_ReUx4sDY4EsUiwenZH_fIx9ivUU4c9&v=1&x=3&a=CAogfCblSMNH6D8P33R2NwBkEpnFGNe4hak-3uefn4pA33kSbRDflJvYxi8Y_4vS2MYvIgEAUgSYfbz6WgTLQFxuaibDfSNLPiyDPu9Wg14Q8f8ZXU3_RCgxT7x_eufaiuwbs080HXBPgnImVM9U7AT6HvGIIXQ2VR0vvcC51ZQs4Jz_IoCooEFA8X4K_xsFgOk&e=1633884079&fl=&r=294a78e8-7218-4b65-bdaf-4d03831879bb-1&k=Dnpx3bpGnrN0vL1CDkGSzA&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=CsQQ1FScG5uW4WSMDNzTVGnvkWY&cd=i',
+    url: '/assets/images/old_school/popup/notice.jpg',
     width: 232,
     height: 350
   },
   {
     name: 'Keep Out',
-    url: 'https://cvws.icloud-content.com/B/ASiZZ27CLB-eSiMZ3IA2lYdp2SvQAf8fBexmJcUgZSgoqSzp5c8iA0CY/RESTRICTED.png?o=AqPv4e0zCayhYdtDuWRuV9XoQ5Q275wpGz358_YcStE1&v=1&x=3&a=CAogLJh7fmG5QGhMbprKg_8eg2FJ_BJvFrFsZlTAR0gVycISbRCPzp_Yxi8Yr8XW2MYvIgEAUgRp2SvQWgQiA0CYaiZKXSh5i7WvDoJyCfkDUH3f-h0s0Px1CR4jDr4KVMbDZTmkqtOHmnImMBHeO2ect3aLLEoBmiwhHiRE3e9MtWiUCGsU-xM7GCdgqEL-nlA&e=1633884152&fl=&r=284f1377-5da3-46a7-b269-ddf724e5b87d-1&k=Bs8oqXslHSqQrecT4YL68A&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=55&s=-06UWcuYf9aCOF-UvH373FKyUOI&cd=i',
+    url: '/assets/images/old_school/popup/RESTRICTED.png',
     width: 264,
     height: 191
   }
