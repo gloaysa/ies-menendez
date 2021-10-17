734b9c748d5917ec1a26f9aae26f7e30a4ce3d71
Guillermo Loaysa <guillermo.loaysa.extern@diconium.com>
2021-10-17 19:08:18 +0200

    fix(): show featured content again

--- src/app/pages/old-school/old-school.page.ts
+++ src/app/pages/old-school/old-school.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import { PageItem } from './page-item.interface';
 
@@ -9,8 +11,22 @@ import { pageItemContent } from './page-item.content';
   templateUrl: './old-school.page.html',
   styleUrls: ['./old-school.page.scss']
 })
-export class OldSchoolPage {
+export class OldSchoolPage implements OnInit {
   selectedItem: PageItem | undefined;
   ulItems: PageItem[] = pageItemContent;
+
+  constructor(private readonly router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd)
+      )
+      .subscribe((event) => {
+        const url = event.url.replace('/', '').trim();
+        this.selectedItem = this.ulItems.find((item) => item.url === url);
+    });
+  }
 
 }
